d445dbb3b1b6ff0f3264d64c6eee8ce0cbe07fc3
Nsajekar <niteshsajekar112@gmail.com>
2024-12-17 21:19:26 +0530

    new db scheme added

--- src/main/java/com/spring/main/config/DataBaseConfig.java
+++ src/main/java/com/spring/main/config/DataBaseConfig.java
@@ -2,32 +2,61 @@ package com.spring.main.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
 @Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class DataBaseConfig {
 	
-	@Value("${spring.mariadb.datasource.url}")
-    private String url;
+	@Value("${spring.datasource.mariadb.url}")
+    String mariaUrl;
+	
+	@Value("${spring.datasource.mariadb.usr}")
+	String mariaUsr;
+	
+	@Value("${spring.datasource.mariadb.pwd}")
+	String mariaPwd;
+	
+	@Value("${spring.datasource.mariadb.driver-class-name}")
+	String mariaDrivarName;
+	
+	@Value("${spring.datasource.mysql.url}")
+    String mysqlUrl;
+	
+	@Value("${spring.datasource.mysql.usr}")
+	String mysqlUsr;
+	
+	@Value("${spring.datasource.mysql.pwd}")
+	String mysqlPwd;
+	
+	@Value("${spring.datasource.mysql.driver-class-name}")
+	String mysqlDrivarName;
 
-    @Value("${spring.mariadb.datasource.driver-class-name}")
-    private String driverClassName;
-
-    @Autowired
-    Environment env;
+	@Value("${app.datasource.active}")
+	String activeDatabase;
 	
     @Bean
-    DataSource dataSource() throws Exception {
+    DataSource dataSource() throws IllegalArgumentException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(env.getProperty("spring.mariadb.datasource.usr"));
-        dataSource.setPassword(env.getProperty("spring.mariadb.datasource.pwd"));
-        dataSource.setDriverClassName(driverClassName);
+        if("MARIADB".equals(activeDatabase)) {
+        	dataSource.setUrl(mariaUrl);
+        	dataSource.setUsername(mariaUsr);
+        	dataSource.setPassword(mariaPwd);
+        	dataSource.setDriverClassName(mariaDrivarName);
+        }else if("MYSQL".equals(activeDatabase)) {
+        	dataSource.setUrl(mysqlUrl);
+        	dataSource.setUsername(mysqlUsr);
+        	dataSource.setPassword(mysqlPwd);
+        	dataSource.setDriverClassName(mysqlDrivarName);
+        }else {
+        	throw new IllegalArgumentException("Unsupported Database :!" + activeDatabase);
+        }
         return dataSource;
     }
 }
