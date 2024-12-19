15e0cd5014f8e9e3bed1fdefe6cfe8edf8283dc9
Nsajekar <niteshsajekar112@gmail.com>
2024-12-19 16:01:44 +0530

    LOGS WORKING NOW

--- src/main/java/com/spring/main/SpringBootProjectApplication.java
+++ src/main/java/com/spring/main/SpringBootProjectApplication.java
@@ -1,13 +1,28 @@
 package com.spring.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author nsajekar
+ * 
+ * Note :
+ * 1.Before Running Project Add -DLOG_PATH=C:/NITESH/MY_SPACE/SPRING_BOOT/logs To JVM Argument 
+ * 2.Before Building Project Add clean install package -DLOG_PATH=C:/NITESH/MY_SPACE/SPRING_BOOT/logs To Goals
+ */
 @SpringBootApplication
-public class SpringBootProjectApplication {
-
+public class SpringBootProjectApplication{
+	
+	private static final Logger logger = LogManager.getLogger(SpringBootProjectApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
+		logger.info("Application Started..."); 
+		logger.debug("Application Started...");
+		logger.error("Application Started...");
+		logger.warn("Application Started...");
 	}
-	
+
 }
--- src/main/java/com/spring/main/controller/SpringJDBCController.java
+++ src/main/java/com/spring/main/controller/SpringJDBCController.java
@@ -2,12 +2,15 @@ package com.spring.main.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.main.SpringBootProjectApplication;
 import com.spring.main.model.DataBean;
 import com.spring.main.repository.CommonRestDao;
 
@@ -17,12 +20,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @RequestMapping(value = "/spring/jdbc")
 public class SpringJDBCController {
+	
+	private static final Logger logger = LogManager.getLogger(SpringBootProjectApplication.class);
 
 	@Autowired
 	CommonRestDao CommonRestDao;
 
 	@GetMapping(value = "/getdata", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<DataBean> myfirstApi() {
+		logger.info("Method SpringJDBCController.myfirstApi() Started ..");
 		List<DataBean> list = CommonRestDao.getDataList();
 		return list;
 	}
