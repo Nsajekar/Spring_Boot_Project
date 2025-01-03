c62286c6bacab481b8a1814882d8136b0c493dc2
Nsajekar <niteshsajekar112@gmail.com>
2025-01-03 20:29:55 +0530

    Update Student.java

--- src/main/java/com/spring/main/model/entity/Student.java
+++ src/main/java/com/spring/main/model/entity/Student.java
@@ -1,5 +1,12 @@
 package com.spring.main.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="student")
 public class Student {
 
 }
