4f9dc3410c7a8f97ba125fed2bfe5a2c787e04db
Nsajekar <niteshsajekar112@gmail.com>
2025-02-27 17:40:16 +0530

    angular code api added

---
+++ src/main/resources/sql/02_user.sql
@@ -0,0 +1,22 @@
+USE `services_nitesh_db`;
+
+--
+-- Table structure for table `user_table`
+--
+
+DROP TABLE IF EXISTS `user_table`;
+
+
+CREATE TABLE `user_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) DEFAULT NULL,
+  `pass_word` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `student_roll_number` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`, `user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE student
+ADD student_roll_number int NOT NULL DEFAULT 0, 
+ADD is_allowed BOOLEAN NOT NULL;
